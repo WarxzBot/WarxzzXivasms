a53bf7596a8d4c03b332bfe0f36f26e184202d57
WarxzBot <148389366+WarxzBot@users.noreply.github.com>
2026-01-25 23:23:03 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+TOKEN: "8375190418:AAEYEyKJe_lelBlprPYs01xmOu8GXDNFvtI",
+    "BOT_OWNER_LINK": "https://t.me/YantooooTL",
+    "IVAS_USERNAME": "",
+    "IVAS_PASSWORD": "",
+    OWNER_ID: 8439307766,
+    "CHAT_ID": [
+    ],
+    
+CF_CLEARANCE_VALUE: '',
+CURRENT_USER_AGENT: '',
+MAIN_CHANNEL_LINK: 'https://t.me/CHANNELxOXPB',
+NUMBER_GROUP_LINK: 'https://t.me/+1WOYZ1rGGwAzMmY1',
+    
+PROJECT_NAME: "ꪶ あ ꫂ⛧ 𝙬̸𝙖͢𝙧͞𝙭͢𝙯͞𝙯͢ × 𝙄̸𝙫͞𝙖͢𝙨͞𝙢͢𝙨 𝘽̸𝙤͞𝙩̸𝙕͞ "
+  
+};
